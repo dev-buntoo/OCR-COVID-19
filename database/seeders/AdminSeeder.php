884221c70893b1bbd7f7ff5671e1483a463ef1ff
Dev Buntoo <haroon.abid.1999@gmail.com>
2021-08-01 10:39:31 +0500

    Admin Seeder Added + bug fixed:

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $user = User::create([
+            'email' => 'admin@admin.com',
+            'cnic' => '1234567898765',
+            'gender' => '1',
+            'date_of_birth' => '1999-11-15',
+            'password' => Hash::make('11223344'),
+            'role_id' => 1,
+        ]);
+        $admin = DB::table('admins')->insert([
+                'name' => 'ADMIN',
+                'address' => 'Lahore',
+                'city' => 'Lahore',
+                'state' => 'Punjab',
+                'user_id' => $user->id,
+        ]);
+    }
+}
