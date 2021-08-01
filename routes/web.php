06fe871fb034366554f726d1f70f4713002a0f0d
Dev Buntoo <haroon.abid.1999@gmail.com>
2021-08-01 10:49:10 +0500

    Registor Route Removed

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+]);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::name('general.')->group(function(){
